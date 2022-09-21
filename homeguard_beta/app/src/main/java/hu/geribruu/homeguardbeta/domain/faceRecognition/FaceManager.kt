050822424066e302e4ec6cc176aeddbb50c8d55e
Németh Gergely 2 <nemeth.gergely2@autsoft.hu>
2022-09-21 15:44:43 +0200

    Finish face capture algorithm

--- homeguard_beta/app/src/main/java/hu/geribruu/homeguardbeta/domain/faceRecognition/FaceManager.kt
+++ homeguard_beta/app/src/main/java/hu/geribruu/homeguardbeta/domain/faceRecognition/FaceManager.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import android.util.Log
 import hu.geribruu.homeguardbeta.data.face.disk.FaceDiskDataSource
 import hu.geribruu.homeguardbeta.data.history.HistoryRepository
+import hu.geribruu.homeguardbeta.data.history.disk.model.RoomHistoryItem
 import hu.geribruu.homeguardbeta.domain.faceRecognition.model.RecognizedFace
 import hu.geribruu.homeguardbeta.domain.faceRecognition.model.SimilarityClassifier
 import hu.geribruu.homeguardbeta.domain.faceRecognition.util.insertToSP
-import hu.geribruu.homeguardbeta.domain.history.model.FaceCaptureTempItem
+import hu.geribruu.homeguardbeta.domain.history.model.FaceCapturedItem
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
@@ -22,7 +23,7 @@ class FaceManager @Inject constructor(
 ) {
 
     private var detectedFace: MutableList<String> = mutableListOf()
-    private var faceCaptureTempItems: MutableList<FaceCaptureTempItem> = mutableListOf()
+    private var faceCapturedItems: MutableList<FaceCapturedItem> = mutableListOf()
 
     fun manageNewFace(
         registered: HashMap<String?, SimilarityClassifier.Recognition>,
@@ -41,108 +42,73 @@ class FaceManager @Inject constructor(
         }
     }
 
-    fun manageFace(name: String) {
-        val date = SimpleDateFormat(
-            PhotoCapture.FILENAME_FORMAT,
-            Locale.US
-        ).format(System.currentTimeMillis())
+    private fun getFaceCapturedIfInList(name: String): FaceCapturedItem? {
+        faceCapturedItems.forEach { face ->
+            if (face.name == name) {
+                return face
+            }
+        }
+        return null
+    }
 
-        if (name !in detectedFace && name != "Unknown" && name != "No Face Detected!") {
-            detectedFace.add(name)
-            faceCaptureTempItems.add(FaceCaptureTempItem(name))
-            Log.d("asd", "$name is add to detect face")
-            faceCaptureTempItems.forEach { face ->
-                Log.d("asd", "${face.name} is elemnt of facecaptuere")
+    fun manageFace(name: String) {
+        if (name !in detectedFace &&
+            name != "Unknown" &&
+            name != "No Face Detected!" &&
+            name != ""
+        ) {
+            val faceCaptured = getFaceCapturedIfInList(name)
+            if (faceCaptured == null) {
+                detectedFace.add(name)
+                faceCapturedItems.add(FaceCapturedItem(name))
+                getFaceCapturedIfInList(name)?.startTimer()
+            }
+
+            if (faceCaptured != null && faceCaptured.isDetectable) {
+                detectedFace.add(name)
+                faceCaptured.startTimer()
             }
         }
 
-        if (name in detectedFace && name != "Unknown" && name != "No Face Detected!") {
-            Log.d("asd", "$name start timer in facemanager")
-
-            faceCaptureTempItems.forEach { face ->
+        if (name in detectedFace &&
+            name != "Unknown" &&
+            name != "No Face Detected!" &&
+            name != ""
+        ) {
+            faceCapturedItems.forEach { face ->
                 if (face.name == name) {
-                    Log.d("asd", "$name set run or stop timer")
+                    if (!face.isCounterRunning && face.isDetectable) {
+                        face.startTimer()
+                    }
                     face.runTimer()
                 }
             }
         } else {
-            Log.d("asd", "$name stop timer in facemanager")
-            faceCaptureTempItems.forEach { face ->
+            faceCapturedItems.forEach { face ->
                 face.stopTimer()
             }
         }
 
-        faceCaptureTempItems.forEach { face ->
-            if (face.isFinished) {
-                Log.d("asd", "${face.name} VÉGE VÉGZET")
+        faceCapturedItems.forEach { face ->
+            if (face.isDetected) {
+                detectedFace.remove(face.name)
+                saveFaceHistory(face.name)
+                face.reset()
+                face.startAvailableTimer()
             }
         }
+    }
 
-        // ----------------------
+    private fun saveFaceHistory(name: String) {
+        GlobalScope.launch {
+            val date = SimpleDateFormat(
+                PhotoCapture.FILENAME_FORMAT,
+                Locale.US
+            ).format(System.currentTimeMillis())
 
-//        if (name !in detectedFace && name != "Unknown" && name != "No Face Detected") {
-//            detectedFace[name] = 3000
-//
-//            object : CountDownTimer(15000, 1000) {
-//                override fun onTick(p0: Long) {
-//                    Log.d("asd", "$name Remain time: $p0")
-//                }
-//
-//                override fun onFinish() {
-//                    Log.d("asd", "$name finished")
-//                }
-//            }.start()
-//        }
-//
-//        if (name in detectedFace && name != "Unknown" && name != "No Face Detected") {
-//
-//        }
-
-//        if (name !in detectedFace && name != "Unknown" && name != "No Face Detected") {
-//            detectedFace[name] = 3000
-//
-//            object : CountDownTimer(15000, 1000) {
-//                override fun onTick(p0: Long) {
-//                    Log.d("asd", "$name Remain time: $p0")
-//                    if (name in detectedFace) {
-//                        detectedFace[name] = detectedFace[name]?.minus(1000) ?: 3000
-//                    }
-//
-//                    detectedFace.forEach { face ->
-//                        Log.d("asd", "${face.key} timer left: ${face.value}")
-//                    }
-//                }
-//
-//                override fun onFinish() {
-//                    Log.d("asd", "$name finished")
-//                }
-//            }.start()
-//        } else {
-//            Log.d("asd", "Delete: $name")
-//            detectedFace.remove(name)
-//        }
-
-//        detectedFace.forEach { face ->
-//            Log.d("asd", "${face.key} timer left: ${face.value}")
-//        }
-
-//        if (name !in detectedFace) {
-//            detectedFace.put(name, 3000)
-//            val asd = object : CountDownTimer(15000, 1000) {
-//                override fun onTick(p0: Long) {
-//                    Log.d("asd", "$name Remain time: $p0")
-//                }
-//
-//                override fun onFinish() {
-//                    Log.d("asd", "$name finished")
-//                }
-//            }.start()
-//        }
-
-//        GlobalScope.launch {
-//            repositoryHistory.insertHistory(
-//                RoomHistoryItem(0, name, date)
-//            )
-//        }
+            repositoryHistory.insertHistory(
+                RoomHistoryItem(0, name, date)
+            )
+        }
     }
 }
